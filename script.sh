3518f8a8165bc1aab4ea3f8c6a64ed9f1cad76ed
Skye <lewallen4@gmail.com>
2024-04-09 15:44:57 -0500

    Added Cloudsy fix, and added hourly table

--- script.sh
+++ script.sh
@@ -121,7 +121,142 @@ fi
 	
 	currentcond=$(cat "db/7day.json" | grep -m 1 -A 27 '"number": 1,' | grep '"detailedForecast"' | sed 's/^.*"\([^"]*\)"[^"]*$/\1/' | awk '{for(i=1;i<=NF;i++) {printf "%s ",$i;if (i%8==0) printf "<p></p> "};print ""}'
 )
+	# Hours
 	
+	
+hourRawTime1a=$(cat "db/TOD.json" | grep -m 1 startTime | awk -F '[:-T]' '{print $4}')
+hourRawTime2a=$(cat "db/TOD.json" | grep -m 2 startTime | awk -F '[:-T]' '{print $4}' | tail -n 1)
+hourRawTime3a=$(cat "db/TOD.json" | grep -m 3 startTime | awk -F '[:-T]' '{print $4}' | tail -n 1)
+hourRawTime4a=$(cat "db/TOD.json" | grep -m 4 startTime | awk -F '[:-T]' '{print $4}' | tail -n 1)
+hourRawTime5a=$(cat "db/TOD.json" | grep -m 5 startTime | awk -F '[:-T]' '{print $4}' | tail -n 1)
+hourRawTime6a=$(cat "db/TOD.json" | grep -m 6 startTime | awk -F '[:-T]' '{print $4}' | tail -n 1)
+hourRawTime7a=$(cat "db/TOD.json" | grep -m 7 startTime | awk -F '[:-T]' '{print $4}' | tail -n 1)
+hourRawTime8a=$(cat "db/TOD.json" | grep -m 8 startTime | awk -F '[:-T]' '{print $4}' | tail -n 1)
+hourRawTime9a=$(cat "db/TOD.json" | grep -m 9 startTime | awk -F '[:-T]' '{print $4}' | tail -n 1)
+hourRawTime10a=$(cat "db/TOD.json" | grep -m 10 startTime | awk -F '[:-T]' '{print $4}' | tail -n 1)
+hourRawTime11a=$(cat "db/TOD.json" | grep -m 11 startTime | awk -F '[:-T]' '{print $4}' | tail -n 1)
+hourRawTime12a=$(cat "db/TOD.json" | grep -m 12 startTime | awk -F '[:-T]' '{print $4}' | tail -n 1)
+hourRawTime13a=$(cat "db/TOD.json" | grep -m 13 startTime | awk -F '[:-T]' '{print $4}' | tail -n 1)
+hourRawTime14a=$(cat "db/TOD.json" | grep -m 14 startTime | awk -F '[:-T]' '{print $4}' | tail -n 1)
+hourRawTime15a=$(cat "db/TOD.json" | grep -m 15 startTime | awk -F '[:-T]' '{print $4}' | tail -n 1)
+hourRawTime16a=$(cat "db/TOD.json" | grep -m 16 startTime | awk -F '[:-T]' '{print $4}' | tail -n 1)
+hourRawTime17a=$(cat "db/TOD.json" | grep -m 17 startTime | awk -F '[:-T]' '{print $4}' | tail -n 1)
+hourRawTime18a=$(cat "db/TOD.json" | grep -m 18 startTime | awk -F '[:-T]' '{print $4}' | tail -n 1)
+hourRawTime19a=$(cat "db/TOD.json" | grep -m 19 startTime | awk -F '[:-T]' '{print $4}' | tail -n 1)
+hourRawTime20a=$(cat "db/TOD.json" | grep -m 20 startTime | awk -F '[:-T]' '{print $4}' | tail -n 1)
+hourRawTime21a=$(cat "db/TOD.json" | grep -m 21 startTime | awk -F '[:-T]' '{print $4}' | tail -n 1)
+hourRawTime22a=$(cat "db/TOD.json" | grep -m 22 startTime | awk -F '[:-T]' '{print $4}' | tail -n 1)
+hourRawTime23a=$(cat "db/TOD.json" | grep -m 23 startTime | awk -F '[:-T]' '{print $4}' | tail -n 1)
+hourRawTime24a=$(cat "db/TOD.json" | grep -m 24 startTime | awk -F '[:-T]' '{print $4}' | tail -n 1)
+hourRawTime25a=$(cat "db/TOD.json" | grep -m 25 startTime | awk -F '[:-T]' '{print $4}' | tail -n 1)
+
+for i in {1..25}; do
+	eval hourTemp${i}a=$(cat 'db/TOD.json' | grep -m $i '"temperature":' | awk -F '[:]' '{print $2}' | awk -F ',' '{print $1}' | awk -F '[ ]' '{print $2}' | tail -n 1)
+done
+
+for i in {1..25}; do
+	eval hourRain${i}a=$(cat 'db/TOD.json' | grep -m $i -A 2 '"probabilityOfPrecipitation' | awk -F '[:]' '{print $2}' | awk -F ',' '{print $1}' | awk -F '[ ]' '{print $2}' | tail -n 1)
+done
+
+
+
+	
+convert_to_12_hour_format() {
+    local hour=$1
+
+    # Remove leading zeros if present
+    hour=${hour#0}
+
+    if (( hour > 12 )); then
+        hour=$((hour - 12))
+        suffix="pm"
+    else
+        suffix="am"
+    fi
+
+    echo "$hour:00$suffix"
+}
+
+# Generate Times
+
+hour=$hourRawTime1a
+hourTime1=$(convert_to_12_hour_format "$hour")
+
+hour=$hourRawTime2a
+hourTime2=$(convert_to_12_hour_format "$hour")
+
+hour=$hourRawTime3a
+hourTime3=$(convert_to_12_hour_format "$hour")
+
+hour=$hourRawTime4a
+hourTime4=$(convert_to_12_hour_format "$hour")
+
+hour=$hourRawTime5a
+hourTime5=$(convert_to_12_hour_format "$hour")
+
+hour=$hourRawTime6a
+hourTime6=$(convert_to_12_hour_format "$hour")
+
+hour=$hourRawTime7a
+hourTime7=$(convert_to_12_hour_format "$hour")
+
+hour=$hourRawTime8a
+hourTime8=$(convert_to_12_hour_format "$hour")
+
+hour=$hourRawTime9a
+hourTime9=$(convert_to_12_hour_format "$hour")
+
+hour=$hourRawTime10a
+hourTime10=$(convert_to_12_hour_format "$hour")
+
+hour=$hourRawTime11a
+hourTime11=$(convert_to_12_hour_format "$hour")
+
+hour=$hourRawTime12a
+hourTime12=$(convert_to_12_hour_format "$hour")
+
+hour=$hourRawTime13a
+hourTime13=$(convert_to_12_hour_format "$hour")
+
+hour=$hourRawTime14a
+hourTime14=$(convert_to_12_hour_format "$hour")
+
+hour=$hourRawTime15a
+hourTime15=$(convert_to_12_hour_format "$hour")
+
+hour=$hourRawTime16a
+hourTime16=$(convert_to_12_hour_format "$hour")
+
+hour=$hourRawTime17a
+hourTime17=$(convert_to_12_hour_format "$hour")
+
+hour=$hourRawTime18a
+hourTime18=$(convert_to_12_hour_format "$hour")
+
+hour=$hourRawTime19a
+hourTime19=$(convert_to_12_hour_format "$hour")
+
+hour=$hourRawTime20a
+hourTime20=$(convert_to_12_hour_format "$hour")
+
+hour=$hourRawTime21a
+hourTime21=$(convert_to_12_hour_format "$hour")
+
+hour=$hourRawTime22a
+hourTime22=$(convert_to_12_hour_format "$hour")
+
+hour=$hourRawTime23a
+hourTime23=$(convert_to_12_hour_format "$hour")
+
+hour=$hourRawTime24a
+hourTime24=$(convert_to_12_hour_format "$hour")
+
+hour=$hourRawTime25a
+hourTime25=$(convert_to_12_hour_format "$hour")
+
+	
+
+
 	
 	
 	
@@ -146,7 +281,7 @@ fi
 	
 
     # Write HTML content to a file
-    cat <<EOF >db/frontEnd.html
+    cat <<EOF >db/frontEndraw.html
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -169,17 +304,69 @@ fi
             border: 5px solid white;
             padding: 20px;
             border-radius: 10px;
+            overflow: auto; /* Add overflow: auto; to enable scrolling if content overflows */
+			margin-top: 1025px; /* Add negative margin-top to extend scrolling slightly above the top */
+            padding-top: 1px; /* Add padding-top to compensate for negative margin-top */
+        
         }
         img {
             max-width: 100%;
-            height: auto;
+            max-height: 100%; /* Add max-height: 100%; to ensure the image fits within the container */
+            margin-bottom: 20px; /* Add margin-bottom to create space between the image and other content */
+        }
+        .table-container {
+            display: block;
+            text-align: center;
+            margin-top: 40px; /* Adjusted margin here */
+        }
+
+        table {
+            width: 400px;
+            border-collapse: collapse;
+            overflow: hidden;
+            box-shadow: 0 0 20px rgba(0,0,0,0.1);
+            margin: 0 auto;
+        }
+
+        th,
+        td {
+            padding: 10px;
+            background-color: rgba(255,255,255,0.2);
+            color: #000;
+        }
+
+        th {
+            text-align: left;
+        }
+
+        thead th {
+            background-color: #55608f;
+        }
+
+        tbody tr:hover {
+            background-color: rgba(200,200,200,0.3);
+        }
+
+        td {
+            position: relative;
+        }
+
+        td:hover:before {
+            content: "";
+            position: absolute;
+            left: 0;
+            right: 0;
+            top: -9999px;
+            bottom: -9999px;
+            background-color: rgba(255,255,255,0.2);
+            z-index: -1;
         }
     </style>
 </head>
 <body>
     <div class="container">
-		
-<p><img src="logo.gif" alt="Your Logo" /></p>
+	
+        <p><img src="logo.gif" alt="Your Logo"></p>
 
         <h3>NOAA Information</h3>
         <p><strong>City Name:</strong> $currentcity</p>
@@ -187,14 +374,171 @@ fi
         <p><strong>Current Temp:</strong> $currenttemp°F</p>
         <p><strong>Radar Station:</strong> $currentstation</p>
         <img src="radar.gif" alt="Radar">
-		<p>$currentcond</p>
+        <p>$currentcond</p>
+
+        <h2><p style="margin-top: 40px;">Hourly Forecast</p></h2>
+<table border="1">
+    <tr>
+        <td>Local Time</td>
+        <td>Temperature</td>
+        <td>Change of Rain</td>
+    </tr>
+    <tr>
+        <td>$hourTime1</td>
+        <td>$hourTemp1a°F</td>
+        <td>$hourRain1a%</td>
+    </tr>
+    <tr>
+        <td>$hourTime2</td>
+        <td>$hourTemp2a°F</td>
+        <td>$hourRain2a%</td>
+    </tr>
+    <tr>
+        <td>$hourTime3</td>
+        <td>$hourTemp3a°F</td>
+        <td>$hourRain3a%</td>
+    </tr>
+    <tr>
+        <td>$hourTime4</td>
+        <td>$hourTemp4a°F</td>
+        <td>$hourRain4a%</td>
+    </tr>
+	    <tr>
+        <td>$hourTime5</td>
+        <td>$hourTemp5a°F</td>
+        <td>$hourRain5a%</td>
+    </tr>
+	    <tr>
+        <td>$hourTime6</td>
+        <td>$hourTemp6a°F</td>
+        <td>$hourRain6a%</td>
+    </tr>
+	    <tr>
+        <td>$hourTime7</td>
+        <td>$hourTemp7a°F</td>
+        <td>$hourRain7a%</td>
+    </tr>
+	    <tr>
+        <td>$hourTime8</td>
+        <td>$hourTemp8a°F</td>
+        <td>$hourRain8a%</td>
+    </tr>
+	    <tr>
+        <td>$hourTime9</td>
+        <td>$hourTemp9a°F</td>
+        <td>$hourRain9a%</td>
+    </tr>
+	    <tr>
+        <td>$hourTime10</td>
+        <td>$hourTemp10a°F</td>
+        <td>$hourRain10a%</td>
+    </tr>
+	    <tr>
+        <td>$hourTime11</td>
+        <td>$hourTemp11a°F</td>
+        <td>$hourRain11a%</td>
+    </tr>
+	    <tr>
+        <td>$hourTime12</td>
+        <td>$hourTemp12a°F</td>
+        <td>$hourRain12a%</td>
+    </tr>
+	    <tr>
+        <td>$hourTime13</td>
+        <td>$hourTemp13a°F</td>
+        <td>$hourRain13a%</td>
+    </tr>
+	    <tr>
+        <td>$hourTime14</td>
+        <td>$hourTemp14a°F</td>
+        <td>$hourRain14a%</td>
+    </tr>
+	    <tr>
+        <td>$hourTime15</td>
+        <td>$hourTemp15a°F</td>
+        <td>$hourRain15a%</td>
+    </tr>
+	    <tr>
+        <td>$hourTime16</td>
+        <td>$hourTemp16a°F</td>
+        <td>$hourRain16a%</td>
+    </tr>
+	    <tr>
+        <td>$hourTime17</td>
+        <td>$hourTemp17a°F</td>
+        <td>$hourRain17a%</td>
+    </tr>
+	    <tr>
+        <td>$hourTime18</td>
+        <td>$hourTemp18a°F</td>
+        <td>$hourRain18a%</td>
+    </tr>
+	    <tr>
+        <td>$hourTime19</td>
+        <td>$hourTemp19a°F</td>
+        <td>$hourRain19a%</td>
+    </tr>
+	    <tr>
+        <td>$hourTime20</td>
+        <td>$hourTemp20a°F</td>
+        <td>$hourRain20a%</td>
+    </tr>
+	    <tr>
+        <td>$hourTime21</td>
+        <td>$hourTemp21a°F</td>
+        <td>$hourRain21a%</td>
+    </tr>
+	    <tr>
+        <td>$hourTime22</td>
+        <td>$hourTemp22a°F</td>
+        <td>$hourRain22a%</td>
+    </tr>
+	    <tr>
+        <td>$hourTime23</td>
+        <td>$hourTemp23a°F</td>
+        <td>$hourRain23a%</td>
+    </tr>
+	    <tr>
+        <td>$hourTime24</td>
+        <td>$hourTemp24a°F</td>
+        <td>$hourRain24a%</td>
+    </tr>
+	    <tr>
+        <td>$hourTime25</td>
+        <td>$hourTemp25a°F</td>
+        <td>$hourRain25a%</td>
+    </tr>
+</table>
+        </div>
     </div>
 </body>
 </html>
+
+
 EOF
 
 
+
+
+# fix the words
+
+awk '{if (gsub("Cloudy", "Cloudsy")) print; else print $0}' db/frontEndraw.html > db/frontEnd.html
+awk '{if (gsub("cloudy", "cloudsy")) print; else print $0}' db/frontEndraw.html > db/frontEnd.html
+
+
+
+
+
+
+# open HTML doc
+
 powershell -Command "Start-Process 'db/frontEnd.html' -WindowStyle Maximized"
+
+
+
+
+
+
 
     # Wait for 600 seconds (10 minutes) before fetching data again
 	echo "  will refresh in 10 minutes..."
